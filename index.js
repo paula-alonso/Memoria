75a07e9354d1ff7a6a0446471f7755b4be13d110
Paula Alonso <paula.alonso@correo.unimet.edu.ve>
2023-05-19 22:14:14 -0400

    aja

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("a")
+const ButtonElement = document.getElementById("jugar");
+let tablero = document.getElementById("tablero")
+console.log(hola)
+
+function addcards(tablero){
+    for(let i=1; i<=18; i++){
+        tablero.appendChild(`<div class="card" id="card${i}">
+        <div class="front">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/1/1e/Unimet_logo.png" id="cover">
+        </div>
+        <div class="back">
+            <h1>B</h1>
+        </div>
+        </div>`);
+    };
+};
+console.log(addcards(tablero))
+
+ButtonElement.addEventListener("click", console.log("hola"))
