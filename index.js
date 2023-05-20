02c88ff3aa429bda3cfe1ad33faeaa173f2124f1
Paula Alonso <paula.alonso@correo.unimet.edu.ve>
2023-05-20 12:16:45 -0400

    empezando el timer

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 
+document.getElementById("fin-juego").style.visibility = "hidden";
 const ButtomElement= document.getElementById("botonJ");
-
-const buttom = document.getElementById("jugar");
+const buttomPlayAgain= document.getElementById("jugar");
 let tab = document.getElementById("tablero");
-let contFlipped;
+let contFlipped = 0;
 
+let sel = []
 
-const ims = [`https://www.unimet.edu.ve/wp-content/uploads/2020/10/IMG_6210-741x1030.jpg`,
+ButtomElement.addEventListener("click", ()=>{
+    const ims = [`https://www.unimet.edu.ve/wp-content/uploads/2020/10/IMG_6210-741x1030.jpg`,
             `https://pbs.twimg.com/media/EzIDzIOWQAEU0Tq.jpg:large`,
             `https://pbs.twimg.com/media/DgI8uGgX4AA4XkR.jpg`,
             `https://photos.wikimapia.org/p/00/02/08/21/90_big.jpg`,
@@ -14,17 +16,13 @@ const ims = [`https://www.unimet.edu.ve/wp-content/uploads/2020/10/IMG_6210-741x
             `https://www2022.unimet.edu.ve/wp-content/uploads/conocenos-main.jpg`,
             `https://4.bp.blogspot.com/-gFoWLdF8utk/WgWo9rPMN8I/AAAAAAAATC0/XdWfuwR9UXwoVDY9Wpkg2FBfaoCixkVMACLcBGAs/s1600/20140927_160423.jpg`,
             `https://fastly.4sqi.net/img/general/600x600/PlFG4UCZ11hgJbYkzNrfLffjt8-OAuI4nUPcMqivT2E.jpg`]
-
-let sel = []
-
-ButtomElement.addEventListener("click", ()=>{
     let InputElement = document.getElementById("username").value
     contFlipped = 0;
     if(InputElement != ""){
     let cards = []
     for(let i = 0; i<16; i++){
         cards.push(`<div class="card-area" onclick="seleccionartarjeta(${i})">
-        <div class="card" id="card${i}" onclick="selection(${i})">
+        <div class="card" id="card${i}">
             <div class="face front">
                 <img src="https://upload.wikimedia.org/wikipedia/commons/1/1e/Unimet_logo.png" id="cover">
             </div>
@@ -39,8 +37,11 @@ ButtomElement.addEventListener("click", ()=>{
         }
         document.getElementById("user-info").style.visibility = "hidden";
     };
+    document.getElementById("tablero").style= "visible";
+    console.log(cards)
     cards.sort(()=>Math.random()-0.5)
-    tablero.innerHTML = cards.join(" ")
+    tab.innerHTML = cards.join(" ")
+    console.log(tab.innerHTML = cards.join(" "))
 }
 });
 
@@ -69,8 +70,21 @@ function deselec(selec){
             card2.style.transform = "rotateY(0deg)"
         }else{
             contFlipped+=2
+            console.log(contFlipped);  
+            if(contFlipped==16){
+                document.getElementById("fin-juego").style.visibility = "visible";
+            }
         }
     }, 1000);
 }
+
+buttomPlayAgain.addEventListener("click", ()=>{
+    
+    document.getElementById("tablero").style.visibility = "hidden";
+    document.getElementById("fin-juego").style.visibility = "hidden";
+    document.getElementById("username").value = "";
+    document.getElementById("user-info").style.visibility = "visible";
+
+})
 
 
